733a452eb57958a7a99ac1dffad8486457820202
Venzard <assasin0084@gmail.com>
2021-11-04 05:00:00 +0200

    Rout revamp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,51 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {NavbarComponent} from './navbar/navbar.component';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {RouterModule, Routes } from '@angular/router';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatListModule} from "@angular/material/list";
+import {MatTabsModule} from "@angular/material/tabs";
 
+const routes: Routes =[
+  {
+    path:'',
+    redirectTo:'table',
+    pathMatch:'full'
+
+  },
+  {
+    path:'table',
+    loadChildren: () => import('./table/table.module').then(m => m.TableModule),
+
+  },
+  {
+    path:'statistics',
+    loadChildren: () => import('./statistics/statistics.module').then(m => m.StatisticsModule),
+
+  }
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
+
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes),
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    MatTabsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
